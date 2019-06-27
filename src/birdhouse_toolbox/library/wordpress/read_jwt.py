492b3e38032f369059a5bd112ac9248b762e58f7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:19:02 -0400

    fix: Adds default value for arg.

--- src/birdhouse_toolbox/library/wordpress/read_jwt.py
+++ src/birdhouse_toolbox/library/wordpress/read_jwt.py
@@ -3,12 +3,13 @@ import json
 from json import JSONDecodeError
 
 from ..read_config import read_config
+from ...settings import DEFAULT_CONFIG_FILE_NAME
 
 MISSING_CONFIG_MESSAGE = "No wordpress config for {site_url}"
 MISSING_JWT_MESSAGE = "No wordpress jwt for {site_url}"
 
 
-def read_jwt(site_url, config_file_name=None):
+def read_jwt(site_url, config_file_name=DEFAULT_CONFIG_FILE_NAME):
     config = read_config(site_url, config_file_name)
     wordpress = config.get("wordpress", None)
     if wordpress is None:
