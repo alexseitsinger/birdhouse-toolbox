7bbc6bfb7a45eba1f24084bf97800e1de8e6baf2
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:14:55 -0400

    refactor: Renames url arg to site_url.

--- src/birdhouse_toolbox/library/wordpress/commands/authenticate.py
+++ src/birdhouse_toolbox/library/wordpress/commands/authenticate.py
@@ -4,11 +4,11 @@ from ..get_jwt import get_jwt
 from ..save_jwt import save_jwt
 
 
-def authenticate(url, username=None, password=None):
+def authenticate(site_url, username=None, password=None):
     if username is None and password is None:
-        credentials = read_credentials(url)
+        credentials = read_credentials(site_url)
         username = credentials["username"]
         password = credentials["password"]
     else:
-        save_credentials(url, username, password)
-    save_jwt(url, get_jwt(url, username, password))
+        save_credentials(site_url, username, password)
+    save_jwt(site_url, get_jwt(site_url, username, password))
