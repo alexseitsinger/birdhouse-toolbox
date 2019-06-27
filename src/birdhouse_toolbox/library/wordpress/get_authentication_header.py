6f0222a4b6d2375d864427921bf4cdccf559f0af
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:16:31 -0400

    refactor: Adds exception instead of return dict.

--- src/birdhouse_toolbox/library/wordpress/get_authentication_header.py
+++ src/birdhouse_toolbox/library/wordpress/get_authentication_header.py
@@ -5,5 +5,5 @@ def get_authentication_header(site_url):
     jwt = read_jwt(site_url)
     token = jwt.get("token", None)
     if token is None:
-        return {}
+        raise AttributeError("There is no authentication token.")
     return {"Authorization": "Bearer " + token}
