04b0f39f985784884b8adff72019c61604dbb392
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:18:43 -0400

    fix: Renames settings variable.

--- src/birdhouse_toolbox/library/wordpress/get_post_content.py
+++ src/birdhouse_toolbox/library/wordpress/get_post_content.py
@@ -1,10 +1,10 @@
 from ..make_url import make_url
 from ..request import request
-from ...settings import WORDPRESS_POSTS_URL, DEFAULT_REQUEST_TIMEOUT
+from ...settings import WORDPRESS_POSTS_URI, DEFAULT_REQUEST_TIMEOUT
 
 
 def get_post_content(site_url, post_number=0):
-    url = make_url(site_url, WORDPRESS_POSTS_URL)
+    url = make_url(site_url, WORDPRESS_POSTS_URI)
     response = request("get", url)
     content = response[post_number]["content"]["rendered"]
     return content
