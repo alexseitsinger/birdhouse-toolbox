3f17c24a7e2eb05173b2ae32c310c1ba2ca2c1c2
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:17:02 -0400

    fix: Renames settings variable.

--- src/birdhouse_toolbox/library/wordpress/get_jwt.py
+++ src/birdhouse_toolbox/library/wordpress/get_jwt.py
@@ -1,10 +1,10 @@
 from ..request import request
 from ..make_url import make_url
-from ...settings import WORDPRESS_JWT_URL
+from ...settings import WORDPRESS_JWT_URI
 
 
 def get_jwt(site_url, username, password):
-    url = make_url(site_url, WORDPRESS_JWT_URL, "token")
+    url = make_url(site_url, WORDPRESS_JWT_URI, "token")
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     data = {"username": username, "password": password}
     return request("post", url, headers=headers, data=data)
