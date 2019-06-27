1697266d2ef6b17c19b55a91a6f0a7f22d821bd4
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 15:12:42 -0400

    fix: Creates config file if none exists.

--- src/birdhouse_toolbox/library/read_config.py
+++ src/birdhouse_toolbox/library/read_config.py
@@ -7,13 +7,14 @@ from ..settings import DEFAULT_CONFIG_FILE_NAME
 
 
 def read_config(site_url, config_file_name=DEFAULT_CONFIG_FILE_NAME):
-    path = os.path.join(os.path.expanduser("~"), config_file_name)
-    if not os.path.isfile(path):
-        raise FileNotFoundError("Config file not found.")
+    config_file_path = os.path.join(os.path.expanduser("~"), config_file_name)
+    if not os.path.exists(config_file_path):
+        with open(config_file_path, "w") as f:
+            f.write("{}")
     try:
-        data = json.loads(read_file(path))
+        data = json.loads(read_file(config_file_path))
     except JSONDecodeError:
         data = {}
     if site_url not in data:
-        AttributeError("There is no config for {}.".format(site_url))
+        raise AttributeError("There is no config for {}.".format(site_url))
     return data[site_url]
