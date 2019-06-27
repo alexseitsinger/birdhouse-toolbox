f6812507057b6532aa29a01a0915ab96b1c297d7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 17:45:33 -0400

    fix: Adds authentication header to request.

--- src/birdhouse_toolbox/library/wordpress/create_featured_media.py
+++ src/birdhouse_toolbox/library/wordpress/create_featured_media.py
@@ -3,7 +3,7 @@ import os
 from ..request import request
 from ..make_url import make_url
 from ...settings import WORDPRESS_MEDIA_URI, CONTENT_TYPES_ALLOWED
-
+from .get_authentication_header import get_authentication_header
 
 INVALID_CONTENT_TYPE_MESSAGE = "Image must be one of {}"
 
@@ -17,11 +17,14 @@ def create_featured_media(site_url, file_path):
         raise RuntimeError(
             INVALID_CONTENT_TYPE_MESSAGE.format(", ".join(CONTENT_TYPES_ALLOWED.keys()))
         )
-    headers = {
-        "Cache-Control": "no-cache",
-        "Content-Disposition": "attachment; filename={}".format(file_name),
-        "Content-Type": CONTENT_TYPES_ALLOWED[ext],
-    }
+    headers = get_authentication_header(site_url)
+    headers.update(
+        {
+            "Cache-Control": "no-cache",
+            "Content-Disposition": "attachment; filename={}".format(file_name),
+            "Content-Type": CONTENT_TYPES_ALLOWED[ext],
+        }
+    )
     url = make_url(site_url, WORDPRESS_MEDIA_URI)
     with open(file_path, "rb") as f:
         return request("post", url, data=f, headers=headers)
